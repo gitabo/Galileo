0e34db74b474e276c4b6e37e7fdb955ee72cfa00
Mauro <mocci89@hotmail.com>
2018-10-07 14:50:11 +0200

    first commit

--- app/src/main/java/com/example/utente10/galileo/MainActivity.java
+++ app/src/main/java/com/example/utente10/galileo/MainActivity.java
@@ -1,17 +1,24 @@
 package com.example.utente10.galileo;
 
+import android.app.ActionBar;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Toolbar toolbar;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+
+    //private TextView mTextMessage;
+
+     /*private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
@@ -29,13 +36,28 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         }
-    };
+    };*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+
+
+
     }
 
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        drawerlayout.openDrawer(GravityCompat.START);
+        return super.onOptionsItemSelected(item);
+    }*/
 }
